0fe223c40c49a127339a72f9e3594cf72d2be7bf
DungTran <tranmanhdung1609@gmail.com>
2023-02-01 11:30:13 +0700

    updated

--- resources/views/admin/User/list_user.blade.php
+++ resources/views/admin/User/list_user.blade.php
@@ -2,7 +2,7 @@
 @section('admin_content')
 
     <!-- Content -->
-
+<h1>duy ngu</h1>
     <div class="container-xxl flex-grow-1 container-p-y">
       <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light"></span> List Users</h4>
 
