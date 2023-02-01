7d1f839581e3c69778b0f35c5d3d6aefa09a3f51
DungTran <tranmanhdung1609@gmail.com>
2023-02-01 11:32:05 +0700

    updated

--- resources/views/admin/User/list_user.blade.php
+++ resources/views/admin/User/list_user.blade.php
@@ -2,7 +2,7 @@
 @section('admin_content')
 
     <!-- Content -->
-<h1>duy ngu</h1>
+
     <div class="container-xxl flex-grow-1 container-p-y">
       <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light"></span> List Users</h4>
 
@@ -13,6 +13,7 @@
           <table id="table_id" class="table">
             <thead>
               <tr>
+                <th>abc</th>
                 <th>Id</th>
                 <th>Name</th>
                 <th>Email</th>
